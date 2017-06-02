445a910c2ce5564881295c2eac8e2fc4f001f732
Dan Tennery-Spalding <teachrdan@gmail.com>
2017-06-01 18:18:40 -0700

    wrote and passed all tests?

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,16 +1,93 @@
+/*jshint esversion: 6 */
+
 describe("Gilded Rose", function() {
 
-  it("should handle fixed value of 80 for Sulfuras ", function() {
-    items = [ new Item("Sulfuras, Hand of Ragnaros", 80, 0) ];
+  it("should handle fixed quality of 80 for Sulfuras ", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", 10, 80) ];
     update_quality();
     expect(items[0].quality).toEqual(80);
   });
 
-  it("should handle no sell in value for Sulfuras ", function() {
-    items = [ new Item("Sulfuras, Hand of Ragnaros", 80, 0) ];
+  it("should handle no sell in quality for Sulfuras ", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", 10, 80) ];
     update_quality();
-    const isNaN = Object.is(items[0].sell_in, NaN)
-    expect(isNaN).toEqual(true);
+    expect(items[0].sell_in).toEqual(10);
   });
 
+  it("should handle increased quality of Aged Brie", function() {
+    items = [ new Item("Aged Brie", 10, 40) ];
+    update_quality();
+    expect(items[0].quality).toEqual(41);
+  });
+
+  it("should handle increased quality of Aged Brie after sell in date", function() {
+    items = [ new Item("Aged Brie", -1, 40) ];
+    update_quality();
+    expect(items[0].quality).toEqual(41);
+  });
+
+  it("should handle increased by two quality of backstage passes when fewer than 11 days remain", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 11, 40) ];
+    update_quality();
+    expect(items[0].quality).toEqual(42);
+  });
+
+  it("should handle increased by three quality of backstage passes when fewer than 5 days remain", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40) ];
+    update_quality();
+    expect(items[0].quality).toEqual(43);
+  });
+
+  it("should handle zero quality of backstage passes after sell in date", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 40) ];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it("should handle neither Aged Brie nor Backstage Passes becoming greater than 50 quality ", function() {
+    items = [
+      new Item("Aged Brie", 0, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)
+    ];
+    update_quality();
+    var result = items.filter(item => item.quality > 50);
+    expect(result.length).toEqual(0);
+  });
+
+  // NOTE: "Normal" here means not Sulfuras, Aged Brie, Backstage Passes, or Conjured
+  it("should handle a normal item decreasing in quality by one before sell in is 0", function() {
+    items = [ new Item("Eggs", 15, 10) ];
+    update_quality();
+    expect(items[0].quality).toEqual(9);
+  });
+
+  it("should handle a normal item decreasing in quality by two after sell in is 0", function() {
+    items = [ new Item("Eggs", 1, 10) ];
+    update_quality();
+    expect(items[0].quality).toEqual(8);
+  });
+
+  it("should handle a normal item not decreasing in quality below 0", function() {
+    items = [ new Item("Eggs", 10, 0) ];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it("should handle a Conjured item decreasing in quality twice as fast before sell in is 0", function() {
+    items = [ new Item("Conjured Eggs", 15, 10) ];
+    update_quality();
+    expect(items[0].quality).toEqual(8);
+  });
+
+  it("should handle a Conjured item decreasing in quality twice as fast after sell in is 0", function() {
+    items = [ new Item("Conjured Eggs", 0, 10) ];
+    update_quality();
+    expect(items[0].quality).toEqual(6);
+  });
+
+  it("should handle a Conjured item not decreasing in quality below 0", function() {
+    items = [ new Item("Conjured Eggs", 10, 0) ];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,61 +1,45 @@
+/*jshint esversion: 6 */
+
 function Item(name, sell_in, quality) {
   this.name = name;
   this.sell_in = sell_in;
   this.quality = quality;
 }
 
-var items = []
+var items = [];
 
-function update_quality() {
-  // General approach: handle exceptional cases first, end with general case
-  for (var i = 0; i < items.length; i++) {
+// NOTE: avoid manually setting values to max of 50 - DRY.
+// LOGIC: Update everything for readability, bc it's dangerous to add new functionality to unreadable code.
+function update_quality() {  // General approach: handle exceptional cases first, end with general case
+  for (let i = 0; i < items.length; i++) {
     // start with exceptional case of Sulfuras, which is unique in value and aging
     if (items[i].name === 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = NaN;
-      items[i].quality = 80;
-    }
-    // Aged Brie and Backstage Passes do not decrement in value like other items do
-    else if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
+      // NOTE: sell_in date does not change
+      items[i].quality = 80; // NOTE: This may be unnecessary if value is ever enforced elsewhere
     } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
+      // everything else decrements a sell in value
+      // LOGIC: Do this before calculating value, bc update_quality is run at the start of a new business day to determine prices
+      items[i].sell_in--;
+      // Aged Brie and Backstage Passes do not decrement in value like other items do
+      if (items[i].name === 'Aged Brie') {
+        items[i].quality = (items[i].quality >= 50) ? 50 : items[i].quality + 1;
+      } else if (items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
+        if (items[i].sell_in <= 0) {
+          items[i].quality = 0;
+        } else if (items[i].sell_in <= 5) {
+          items[i].quality = (items[i].quality + 3 <= 50) ? items[i].quality + 3 : 50;
+        } else if (items[i].sell_in <= 10) {
+          items[i].quality = (items[i].quality + 2 <= 50) ? items[i].quality + 2 : 50;
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          items[i].quality = (items[i].quality + 1 <= 50) ? items[i].quality++ : 50;
         }
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
+        // NOTE: This is for the normal case - items which diminish in quality as sell in decreases
+        let decreaseBy = (items[0].sell_in > 0) ? 1 : 2;
+        // this is where we decrement value by two if item name begins with "Conjured."
+          // putting this on a separate line for readability--to avoid two ternaries on the same line.
+        decreaseBy = (/Conjured /.test(items[i].name)) ? decreaseBy * 2 : decreaseBy;
+        items[i].quality = (items[i].quality - decreaseBy > 0) ? items[i].quality - decreaseBy : 0;
       }
     }
   }
