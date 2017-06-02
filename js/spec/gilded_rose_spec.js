11dc5e4389666d529aacc4455ea0310228d50c97
Dan Tennery-Spalding <teachrdan@gmail.com>
2017-06-01 17:19:01 -0700

    started building and passing tests

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,16 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("should handle fixed value of 80 for Sulfuras ", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", 80, 0) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].quality).toEqual(80);
+  });
+
+  it("should handle no sell in value for Sulfuras ", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", 80, 0) ];
+    update_quality();
+    const isNaN = Object.is(items[0].sell_in, NaN)
+    expect(isNaN).toEqual(true);
   });
 
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -7,8 +7,15 @@ function Item(name, sell_in, quality) {
 var items = []
 
 function update_quality() {
+  // General approach: handle exceptional cases first, end with general case
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    // start with exceptional case of Sulfuras, which is unique in value and aging
+    if (items[i].name === 'Sulfuras, Hand of Ragnaros') {
+      items[i].sell_in = NaN;
+      items[i].quality = 80;
+    }
+    // Aged Brie and Backstage Passes do not decrement in value like other items do
+    else if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
           items[i].quality = items[i].quality - 1
